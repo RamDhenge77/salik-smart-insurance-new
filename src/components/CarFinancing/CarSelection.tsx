64a8f6af5ff509584a3d5f205a5648b23fcc47a1
RamDhenge77 <ram.d@appening.xyz>
2025-05-27 12:49:52 +0530

    FIXED: car images

--- src/components/CarFinancing/CarSelection.tsx
+++ src/components/CarFinancing/CarSelection.tsx
@@ -129,7 +129,7 @@ const CarSelection = () => {
           year: 2023,
           price: 80000,
           image:
-            "/lovable-uploads/Toyota-Camry2.jpg",
+            "/lovable-uploads/hyundai-sonata.jpg",
         },
       ];
       setCars(carData);
